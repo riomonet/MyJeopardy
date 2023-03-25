41e0ce83ecf8884619bfd268bc59913acc30fd8f
riomonet <arizablozki@gmail.com>
2023-03-24 23:23:38 -0400

    add 2 main funcs 2 evt listners: complete and ready to submit

--- jeopardy.js
+++ jeopardy.js
@@ -1,98 +1,87 @@
-// categories is the main data structure for the app; it looks like this:
 
-//  [
-//    { title: "Math",
-//      clues: [
-//        {question: "2+2", answer: 4, showing: null},
-//        {question: "1+1", answer: 2, showing: null}
-//        ...
-//      ],
-//    },
-//    { title: "Literature",
-//      clues: [
-//        {question: "Hamlet Author", answer: "Shakespeare", showing: null},
-//        {question: "Bell Jar Author", answer: "Plath", showing: null},
-//        ...
-//      ],
-//    },
-//    ...
-//  ]
-
-let categories = [];
+const NUM_CATEGORIES = 6;
+const NUM_QUESTIONS = 5;
+let selected = []; //main data structure
+$('#start').on('click', setupAndStart)  // button to start/restart the game
 
 
-/** Get NUM_CATEGORIES random category from API.
- *
- * Returns array of category ids
- */
-
-function getCategoryIds() {
-}
-
-/** Return object with data about a category:
- *
- *  Returns { title: "Math", clues: clue-array }
- *
- * Where clue-array is:
- *   [
- *      {question: "Hamlet Author", answer: "Shakespeare", showing: null},
- *      {question: "Bell Jar Author", answer: "Plath", showing: null},
- *      ...
- *   ]
- */
-
-function getCategory(catId) {
-}
-
-/** Fill the HTML table#jeopardy with the categories & cells for questions.
- *
- * - The <thead> should be filled w/a <tr>, and a <td> for each category
- * - The <tbody> should be filled w/NUM_QUESTIONS_PER_CAT <tr>s,
- *   each with a question for each category in a <td>
- *   (initally, just show a "?" where the question/answer would go.)
- */
-
-async function fillTable() {
-}
-
-/** Handle clicking on a clue: show the question or answer.
- *
- * Uses .showing property on clue to determine what to show:
- * - if currently null, show question & set .showing to "question"
- * - if currently "question", show answer & set .showing to "answer"
- * - if currently "answer", ignore click
- * */
-
-function handleClick(evt) {
-}
-
-/** Wipe the current Jeopardy board, show the loading spinner,
- * and update the button used to fetch data.
- */
-
-function showLoadingView() {
-
-}
-
-/** Remove the loading spinner and update the button used to fetch data. */
-
-function hideLoadingView() {
-}
-
-/** Start game:
- *
- * - get random category Ids
- * - get data for each category
- * - create HTML table
- * */
-
+// this function queries the API, Sets up the main data structure
 async function setupAndStart() {
+
+    //clear table
+    $('table').children().remove();
+    $('table').append('<tr id=header></tr>')
+    selected = [];
+
+
+    // here we filter categories down to a group of NUM_CATEGOREIES and push onto selected
+    let offset = (Math.random() * 20000).toFixed(0); //randomize Pagination of category query
+    let categories = await axios.get(`https://jservice.io/api/categories?count=100&offset=${offset}`);
+    let filtered = categories.data.filter( (data) => data.clues_count > NUM_QUESTIONS); 
+    let reduced = _.sampleSize(filtered, NUM_CATEGORIES);
+
+    for (item of reduced)
+	selected.push(item);
+
+    // here we get NUM_QUESTIONS questions and answers for theac of those cateogires
+    for (let category of selected)
+	category.clue = await axios.get(`https://jservice.io/api/clues?category=${category.id}`);
+
+    //see if you can do this concurrently with axios.
+
+    for (let category of selected){
+	category.clue = _.sampleSize(category.clue.data, NUM_QUESTIONS);
+	for(c in category.clue) {
+	    let {question, answer ,showing = null} = category.clue[c];
+	    category.clue[c] = {question, answer,showing};
+ 	}
+    }
+
+    // here we call the render function
+    createFrontEnd ();
 }
 
-/** On click of start / restart button, set up game. */
+//this function renders the HTML table
+function createFrontEnd () {
+    // fill header row
+    for (let category of selected) {
+	$('#header').append(`<th>${category.title}</th>`);
+    }
 
-// TODO
 
-/** On page load, add event handler for clicking clues */
+    // create NUM_QUESTIONS rows
+    for (let i = 0; i < NUM_QUESTIONS; i++) {
+	$('table').append(`<tr id=row_${i}></tr>`);
+    }
 
-// TODO
+    //create NUM QUESTIONS * NUM_CATEGORIES <td></td>
+    //each td gets a row num and col num attribute
+    for (let i = 0; i < NUM_QUESTIONS; i++)
+	for (let j = 0; j < NUM_CATEGORIES; j++) 
+	    $(`#row_${i}`).append(`<td row=${i} col=${j}>?</td>`);
+
+}
+
+
+
+//  eventListener for a click on a cell
+$('table').on('click' , 'td', function (evt) {
+
+	evt.preventDefault();
+	let col = this.getAttribute('col');
+	let row = this.getAttribute('row');
+	let question = selected[col].clue[row].question;
+	let answer = selected[col].clue[row].answer;
+	if ($(this).text() === '?')
+		$(this).text(question);
+	else if ($(this).text() === question)
+		$(this).html(answer);
+});
+
+
+/*
+-
+-  axios cuncurrent requests?
+-
+*/
+
